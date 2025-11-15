0cc4a58caeb7549fb0d41b74325cb0c21d5763c5
Kylver Paucar <kilverpaucar1@gmail.com>
2025-11-15 18:30:30 -0500

    feat: implementar funcionalidad 'Dónde Voto' con búsqueda por DNI y mapa interactivo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from './shared/components/Navbar'
 import { Footer } from './shared/components/Footer'
 import { HomePage } from './pages/HomePage'
 import Calendario from './pages/Calendario';
+import { DondeVotar } from './pages/DondeVotar';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/calendario" element={<Calendario />} />
+            <Route path="/donde-votar" element={<DondeVotar />} />
             {/* Agregar más rutas aquí */}
           </Routes>
         </main>
