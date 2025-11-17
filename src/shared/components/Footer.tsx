c236b7a71a49046ce6d4edfb4aa1e97ec18d682c
Angelo270204 <angelow.piscis.27@gmail.com>
2025-11-16 19:00:47 -0500

    Correccion de errores

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -15,9 +15,6 @@ export const Footer: React.FC = () => {
         <div className="footer__content">
           {/* Brand Section */}
           <div className="footer__section footer__section--brand">
-            <div className="footer__logo">
-              <Vote size={32} />
-              <span className="footer__logo-text">{t('footer.brand.title')}</span>
             <div className="footer__logo" style={{ margin: 0, padding: 0, lineHeight: 0 }}>
               {/* Logo institucional DecideYa */}
               <img src="/images/banner/logo.jpg" alt="Logo DecideYa Elecciones 2026" style={{ height: 180, margin: 0, padding: 0, display: 'block' }} />
